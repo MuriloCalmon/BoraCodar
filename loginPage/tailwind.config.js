9179518d3db58d9a21a0f3d5d34295052056f561
MuriloCalmon <murilocalmon80@gmail.com>
2023-03-30 15:38:52 -0300

    add animation

--- loginPage/tailwind.config.js
+++ loginPage/tailwind.config.js
@@ -2,7 +2,22 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        divAnimation: 'divAnimation 2s ease',
+        imgAnimation: 'imgAnimation 2s ease'
+      },
+      keyframes: {
+        divAnimation: {
+          '0%': {transform: 'translateY(-800px)'},
+          '100%': {transform: 'translateY(0)'}
+        },
+        imgAnimation: {
+          '0%': {transform: 'translateX(1000px)'},
+          '100%': {transform: 'translateX(0)'}
+        }
+      }
+    },
   },
   plugins: [],
 }
